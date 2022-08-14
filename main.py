391d10436d7ce20f609d5264a0b06a69a548da51
Zarigani <zari220130@gmail.com>
2022-08-14 22:08:48 +0900

    Add new util function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from utils import get_sum
+from utils import get_sum, get_average
 
 if __name__ == "__main__":
-    result = get_sum(4, 6)
-    print(result)
+    print(get_sum(4, 6))
+    print(get_average([2, 3, 5, 6, 8, 9, 12]))
--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 def get_sum(a: int, b: int) -> int:
     return a + b
+
+
+def get_average(nums: list) -> int:
+    return sum(nums) / len(nums)
